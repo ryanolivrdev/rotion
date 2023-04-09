14ffcb9139698c5b43a4d9f2d2ff11dcb8f4fd07
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-04-09 16:58:16 -0300

    feat: configuring layout

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,7 @@
-import { Routes } from './Routes'
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
 import './styles/global.css'
 
+import { Routes } from './Routes'
+
 export function App() {
-  return (
-    <div className="h-screen w-screen text-rotion-100 flex">
-      <Sidebar />
-
-      <div className="flex-1 flex flex-col max-h-screen">
-        <Header />
-
-        <Routes />
-      </div>
-    </div>
-  )
+  return <Routes />
 }
--- src/renderer/src/Routes.tsx
+++ src/renderer/src/Routes.tsx
@@ -1,15 +1,16 @@
 import { Router, Route } from 'electron-router-dom'
 import { Black } from './pages/black'
 import { Document } from './pages/document'
+import { Default } from './pages/layouts/default'
 
 export function Routes() {
   return (
     <Router
       main={
-        <>
+        <Route path="/" element={<Default />}>
           <Route path="/" element={<Black />} />
           <Route path="/document" element={<Document />} />
-        </>
+        </Route>
       }
     />
   )
